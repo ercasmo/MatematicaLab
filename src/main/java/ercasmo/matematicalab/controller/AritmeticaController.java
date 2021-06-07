57fa3f91b460b4c63bbc5c1c124d4c24709b6feb
Ernesto <netocm@gmail.com>
2021-06-07 15:38:47 -0600

    Se agrega la funcionalidad de la suma para cumplir con la tarea MAT-1 del dashboard.

---
+++ src/main/java/ercasmo/matematicalab/controller/AritmeticaController.java
@@ -0,0 +1,19 @@
+package ercasmo.matematicalab.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/aritmetica")
+public class AritmeticaController {
+
+    @GetMapping("/suma/{uno}/dos")
+    public ResponseEntity findStatusByClave(@PathVariable double uno, double dos) {
+        return new ResponseEntity<>(uno+dos, HttpStatus.OK);
+    }
+
+}
