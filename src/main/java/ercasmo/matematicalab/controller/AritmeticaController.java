73193bcb3a1704857b603ade0c43c62244d25f20
Ernesto Cascante <netocm@gmail.com>
2021-06-14 15:31:19 -0600

    Se modifica el tipo de dato para el manejo de la precisión. el endpoint división simple se deja para ejemplificar.

--- src/main/java/ercasmo/matematicalab/controller/AritmeticaController.java
+++ src/main/java/ercasmo/matematicalab/controller/AritmeticaController.java
@@ -7,27 +7,39 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.math.BigDecimal;
+import java.math.MathContext;
+
 @RestController
 @RequestMapping("/aritmetica")
 public class AritmeticaController {
 
-    @GetMapping("/suma/{uno}/dos")
-    public ResponseEntity suma(@PathVariable double uno, double dos) {
-        return new ResponseEntity<>(uno+dos, HttpStatus.OK);
+    @GetMapping("/suma/{uno}/{dos}")
+    public ResponseEntity suma(@PathVariable BigDecimal uno,@PathVariable BigDecimal dos) {
+        BigDecimal resultado=uno.add(dos, MathContext.DECIMAL128);
+        return new ResponseEntity<>(resultado, HttpStatus.OK);
     }
 
-    @GetMapping("/resta/{uno}/dos")
-    public ResponseEntity resta(@PathVariable double uno, double dos) {
-        return new ResponseEntity<>(uno-dos, HttpStatus.OK);
+    @GetMapping("/resta/{uno}/{dos}")
+    public ResponseEntity resta(@PathVariable BigDecimal uno, @PathVariable BigDecimal dos) {
+        BigDecimal resultado=uno.subtract(dos, MathContext.DECIMAL128);
+        return new ResponseEntity<>(resultado, HttpStatus.OK);
     }
 
-    @GetMapping("/multiplicacion/{uno}/dos")
-    public ResponseEntity multiplicacion(@PathVariable double uno, double dos) {
-        return new ResponseEntity<>(uno*dos, HttpStatus.OK);
+    @GetMapping("/multiplicacion/{uno}/{dos}")
+    public ResponseEntity multiplicacion(@PathVariable BigDecimal uno, @PathVariable BigDecimal dos) {
+        BigDecimal resultado=uno.multiply(dos, MathContext.DECIMAL128);
+        return new ResponseEntity<>(resultado, HttpStatus.OK);
     }
 
-    @GetMapping("/division/{uno}/dos")
-    public ResponseEntity division(@PathVariable double uno, double dos) {
+    @GetMapping("/division/{uno}/{dos}")
+    public ResponseEntity division(@PathVariable BigDecimal uno, @PathVariable BigDecimal dos) {
+        BigDecimal resultado=uno.divide(dos, MathContext.DECIMAL128);
+        return new ResponseEntity<>(resultado, HttpStatus.OK);
+    }
+
+    @GetMapping("/divisionSimple/{uno}/{dos}")
+    public ResponseEntity division(@PathVariable double uno, @PathVariable double dos) {
         return new ResponseEntity<>(uno/dos, HttpStatus.OK);
     }
 }
