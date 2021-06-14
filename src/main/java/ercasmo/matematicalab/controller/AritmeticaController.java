93d42192578f1965f424285d28c4666958bb10c3
Ernesto Cascante <netocm@gmail.com>
2021-06-14 16:18:49 -0600

    Se agregan validaciones al proceso de división

--- src/main/java/ercasmo/matematicalab/controller/AritmeticaController.java
+++ src/main/java/ercasmo/matematicalab/controller/AritmeticaController.java
@@ -2,13 +2,15 @@ package ercasmo.matematicalab.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.constraints.NegativeOrZero;
+import javax.validation.constraints.NotBlank;
+import javax.xml.ws.http.HTTPBinding;
 import java.math.BigDecimal;
 import java.math.MathContext;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/aritmetica")
@@ -32,10 +34,17 @@ public class AritmeticaController {
         return new ResponseEntity<>(resultado, HttpStatus.OK);
     }
 
-    @GetMapping("/division/{uno}/{dos}")
-    public ResponseEntity division(@PathVariable BigDecimal uno, @PathVariable BigDecimal dos) {
-        BigDecimal resultado=uno.divide(dos, MathContext.DECIMAL128);
-        return new ResponseEntity<>(resultado, HttpStatus.OK);
+    @GetMapping("/division")
+    public ResponseEntity division(@RequestParam("divisor")   BigDecimal divisor, @RequestParam("dividendo") BigDecimal dividendo) {
+        if(dividendo.compareTo(BigDecimal.ZERO)!=0) {
+            BigDecimal resultado = divisor.divide(dividendo, MathContext.DECIMAL128);
+            return new ResponseEntity<>(resultado, HttpStatus.OK);
+        }else{
+            Map<String,String> errores=new LinkedHashMap<>();
+            errores.put("code",String.valueOf(HttpStatus.BAD_REQUEST));
+            errores.put("message","Dividendo no puede ser cero");
+            return new ResponseEntity<>(errores, HttpStatus.BAD_REQUEST);
+        }
     }
 
     @GetMapping("/divisionSimple/{uno}/{dos}")
