c65fb83d1cd37667ae3fb3544675e8c6d74b842e
Ernesto Cascante <netocm@gmail.com>
2021-06-14 21:40:55 -0600

    Se agregan los test

---
+++ src/test/java/ercasmo/matematicalab/DivisionTest.java
@@ -0,0 +1,27 @@
+package ercasmo.matematicalab;
+
+import ercasmo.matematicalab.service.AirtmeticaService;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.math.BigDecimal;
+
+@RunWith(MockitoJUnitRunner.class)
+public class DivisionTest {
+    @Autowired
+    AirtmeticaService airtmeticaService;
+    @Test
+    public void testDividir() {
+        try {
+            BigDecimal divisor=new BigDecimal("10.0");
+            BigDecimal dividendo=new BigDecimal("10.0");
+            Assert.assertFalse(airtmeticaService.dividir(divisor,dividendo).equals(BigDecimal.ONE));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
